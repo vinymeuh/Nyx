253e91f90b34f78e46879455e9fa908801cb086f
vinymeuh <vinymeuh@gmail.com>
2020-05-10 15:23:44 +0200

    fish setup on Linux

---
+++ dotfiles/Linux/config/fish/config.fish
@@ -0,0 +1,28 @@
+#
+# ~/.config/fish/config.fish for Linux
+#
+
+#. set locale
+set -x LANG fr_FR.UTF-8
+set -x LC_MESSAGES en_US.UTF-8
+
+#. PATH
+set PATH /usr/local/go/bin $PATH
+set PATH (python -m site --user-base)/bin $PATH
+set PATH $HOME/.bin $HOME/go/bin $PATH
+
+#. Login shell
+if status --is-login
+    eval (ssh-agent -c) > /dev/null
+end
+
+#. Interactive shell
+if status --is-interactive
+    set fish_greeting
+
+    set MANPATH /opt/local/share/man \: $MANPATH
+
+    set -x EDITOR vim
+    set -x GIT_EDITOR vim
+    set -x PAGER less
+end
